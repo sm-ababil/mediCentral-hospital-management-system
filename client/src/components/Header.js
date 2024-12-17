1ff85f8f76ad09e6ff71ff3c143439b72ed91542
S. M. Ababil Islam <oti.ababil@gmail.com>
2024-12-18 05:40:08 +0600

    avatar dropdown and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,81 @@
-import React from "react";
-import { Layout, Menu, Button } from "antd";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Layout, Menu, Button, Dropdown } from "antd";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/header.css";
 import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   const location = useLocation();
+  const [userData, setUserData] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState("");
+
+  const getUser = async () => {
+    try {
+      const response = await axios.post(
+        "/api/v1/user/getUser",
+        {},
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.data && response.data.data) {
+        setIsLoggedIn(true);
+        setUserData(response.data.data);
+        setIsLoading(true);
+        const avatarResponse = await axios.post(
+          `/api/v1/user/getAvatar/${response.data.data._id}`
+        );
+        if (avatarResponse.data.success) {
+          setAvatarUrl(avatarResponse.data.avatarUrl);
+        }
+      } else {
+        console.error("Unexpected response structure:", response.data);
+        setIsLoggedIn(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoggedIn(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    setUserData(null);
+    navigate("/login");
+  };
+
+  const menuItems = [
+    {
+      key: "1",
+      label: (
+        <Link to="/user-profile" className="last-name">
+          <span>{userData ? userData.lastname : "Guest"}</span>
+        </Link>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <Button type="link" onClick={handleLogout} className="logout-button">
+          Log out
+        </Button>
+      ),
+    },
+  ];
 
   return (
     <div className="header">
@@ -34,9 +104,23 @@ const Header = () => {
         >
           Find Doctor
         </Link>
-        <Link to="/login">
-          <Button type="primary">Log in</Button>
-        </Link>
+        {isLoggedIn === null ? null : isLoggedIn ? (
+          isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            <Dropdown
+              menu={{ items: menuItems }}
+              trigger={["click"]}
+              className="avatar-dropdown"
+            >
+              <img src={avatarUrl} alt="User Avatar" className="avatar" />
+            </Dropdown>
+          )
+        ) : (
+          <Link to="/login">
+            <Button type="primary">Log in</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
